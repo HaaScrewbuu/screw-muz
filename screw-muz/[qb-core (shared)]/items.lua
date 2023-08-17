0da9f64d35d553f0482b7dc42f0dc85ff3f400ce
Screw <cemloski_44@outlook.com.tr>
2023-08-17 11:40:19 +0300

    Add files via upload

---
+++ screw-muz/[qb-core (shared)]/items.lua
@@ -0,0 +1,5 @@
+-- qb-core > shared > items.lua
+    
+    -- Screw Muz
+    ['muz'] 				            = {['name'] = 'muz', 			  	  	           ['label'] = 'Muz', 			            ['weight'] = 100, 		   ['type'] = 'item', 		  ['image'] = 'muz.png', 			         ['unique'] = false, 	     ['useable'] = true, 	 ['shouldClose'] = false,      ['combinable'] = nil,   ['description'] = 'Muz'},
+	['muzislenmis'] 		            = {['name'] = 'muzislenmis', 			           ['label'] = 'Muzlu Pasta', 	            ['weight'] = 200, 		   ['type'] = 'item', 		  ['image'] = 'muzislenmis.png', 	         ['unique'] = false, 	     ['useable'] = true, 	 ['shouldClose'] = false,      ['combinable'] = nil,   ['description'] = 'İşlenmiş Muz'},
